2a1517ab25eeaf332f3208ca2f9adaedf90b9574
Mateusz <mateusz.jamiolkowski@gmail.com>
2015-12-22 17:55:55 +0100

    dodano angular

--- dataxu/gulpfile.js
+++ dataxu/gulpfile.js
@@ -1,36 +1,49 @@
 
 var gulp =   require('gulp');
 var sass =   require('gulp-sass');
+var concat = require('gulp-concat');
 
 var src = {
   html: './src/index.html',
-  scss: './src/stylesheets/app.scss'
+  scss: './src/stylesheets/app.scss',
+  jsApp: './src/scripts/**/*.js',
+  jsVendor: './node_modules/angular/angular.js'    
 };
-var dest = {
+
+var dist = {
   html: './dist',
-  css: './dist/stylesheets/'
+  css: './dist/stylesheets/',
+  js: './dist/js'    
 };
 
 
 
 gulp.task('views', function() {
   gulp.src([src.html])
-    .pipe(gulp.dest(dest.html))
+    .pipe(gulp.dest(dist.html))
 });
 
 gulp.task('stylesheets', function() {
   gulp.src([src.scss])
     .pipe(sass())
-    .pipe(gulp.dest(dest.css))
+    .pipe(gulp.dest(dist.css))
 });
 
-gulp.task('build', ['views', 'stylesheets']);
+gulp.task('scripts', function() {
+   gulp.src([src.jsVendor, src.jsApp])
+    .pipe(concat('application.js'))
+    .pipe(gulp.dest(dist.js))   
+});
+
+
+
+gulp.task('build', ['views', 'stylesheets', 'scripts']);
 
 gulp.task('watch', function() {
   gulp.watch([src.scss], ['stylesheets']);
   gulp.watch([src.html], ['views']);
+  gulp.watch([src.jsApp], ['scripts']);
+  gulp.watch([src.jsVendor], ['scripts']);
 });
-
-
 
 gulp.task('default', ['build', 'watch']);
